242895e897c47aa986ff2c3504c0250825e96b81
Ben Lu <benjamincanada12@gmail.com>
2023-06-10 15:39:44 -0700

    Login functionality implementation

---
+++ sql/insertions.sql
@@ -0,0 +1,7 @@
+INSERT INTO account VALUES ('blu', 'Ben', 'Lu', 1234, 'client');
+INSERT INTO account VALUES ('kchua', 'Kelsey', 'Chua', 4444, 'client');
+INSERT INTO account VALUES ('rzou', 'Raymond', 'Zou', 1234, 'client');
+INSERT INTO account VALUES ('blu1', 'Ben', 'Lu', 1234, 'employee');
+INSERT INTO account VALUES ('kchua1', 'Kelsey', 'Chua', 4444, 'employee');
+INSERT INTO account VALUES ('rzou1', 'Raymond', 'Zou', 1234, 'employee');
+
