40d6bd7d03289a6d81dd5b50242f706b077d3e36
Ben Lu <benjamincanada12@gmail.com>
2023-06-10 17:02:41 -0700

    Signup functionality implementation

--- sql/insertions.sql
+++ sql/insertions.sql
@@ -5,3 +5,10 @@ INSERT INTO account VALUES ('blu1', 'Ben', 'Lu', 1234, 'employee');
 INSERT INTO account VALUES ('kchua1', 'Kelsey', 'Chua', 4444, 'employee');
 INSERT INTO account VALUES ('rzou1', 'Raymond', 'Zou', 1234, 'employee');
 
+INSERT INTO client VALUES (21, 'blu');
+INSERT INTO client VALUES (20, 'kchua');
+INSERT INTO client VALUES (22, 'rzou');
+
+INSERT INTO employee VALUES ('manager', 'blu1');
+INSERT INTO employee VALUES ('manager', 'kchua1');
+INSERT INTO employee VALUES ('manager', 'rzou1');
--- src/modules/account/routes.js
+++ src/modules/account/routes.js
@@ -3,4 +3,5 @@ import accountController from './controller.js';
 
 export const accountRouter = express.Router();
 
-accountRouter.get('/', accountController.login)
+accountRouter.get('/login', accountController.login)
+accountRouter.get('/signup', accountController.signup)
